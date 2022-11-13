3344fbc363b0db012ecb55f9c91b3e3f0b32072c
Jonathan <jonathanricardopizidoro@gmail.com>
2022-11-13 18:39:10 -0300

    ai mds f domingo

---
+++ global.js
@@ -0,0 +1,19 @@
+const body = document.querySelector("body"),
+sidebar = document.querySelector('.sidebar'),
+toggle = document.querySelector('.toggle'),
+modeSwitch = document.querySelector('.toggle-switch'),
+modeText = document.querySelector('.mode-text');
+
+modeSwitch.addEventListener("click", ()=>{
+    body.classList.toggle("dark");
+    
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light Mode";
+    }else{
+        modeText.innerText = "Dark Mode";
+    }
+});
+
+toggle.addEventListener("click", ()=>{
+    sidebar.classList.toggle("close");
+});
